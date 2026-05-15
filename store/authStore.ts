4e4e2be39f45e5d90dde8eb1f806a885015f3427
comppy <kim1122aass@gmail.com>
2026-05-15 10:57:11 +0900

    [Feat/#5]Feat:zustand store설정 완료

---
+++ store/authStore.ts
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+interface User {
+  userId: string;
+  email: string;
+}
+
+interface AuthState {
+  user: User | null;
+  isAuthenticated: boolean;
+  setUser: (user: User) => void;
+  clearAuth: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  isAuthenticated: false,
+  setUser: (user) => set({ user, isAuthenticated: true }),
+  clearAuth: () => set({ user: null, isAuthenticated: false }),
+}));
